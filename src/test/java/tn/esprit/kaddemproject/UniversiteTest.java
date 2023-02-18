675e958d4e3b81b9d0f52bf2ccce5b2a92fdf684
Amine <mohamedamine.hamdi@esprit.tn>
2023-02-18 12:35:08 +0100

    First commit

---
+++ src/test/java/tn/esprit/kaddemproject/UniversiteTest.java
@@ -0,0 +1,10 @@
+package tn.esprit.kaddemproject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import tn.esprit.kaddemproject.services.IUniversiteService;
+
+public class UniversiteTest {
+    @Autowired
+    IUniversiteService universiteService;
+    
+}
